8c540c507013240fe3469c8683dda9f339d9aab7
hxxu0007 <hxxu0007@zonemail.clpccd.edu>
2024-07-01 20:28:19 +0000

    v4

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ def main():
     longest = shortest = ''
     words = []
     while True:
-        word = input("Enter a word(type 'stop' to finish.):")
+        word = input("Enter a word-at least 2 words(type 'stop' to finish.):")
         if(word.lower() == 'stop'):
             break
+        words.append(word) 
+    
+    if(len(words[0]) <= len(word[1])):
+        shortest = word[0]
+        longest = word[1]
+    else:
+        shortest = word[1]
+        longest = word[0]
+    for i in range()
 
     ########################################
     # Do not delete the return statement
